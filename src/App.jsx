5c04f96465afb016ebac226b842aaa8b24e09752
Suraj Singh <surajsinghdangi07@gmail.com>
2023-10-05 15:37:50 +0530

    route setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import './App.css'
-import Navbar from './components/Navbar'
+import "./App.css";
+import About from "./Pages/About";
+import Home from "./Pages/Home";
+import Body from "./Pages/Body";
+import FAQs from "./Pages/FAQs";
+import Error from "./Pages/Error";
+import Cart from "./Pages/Cart";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <div>
-    <Navbar/>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="/" element={<Body />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/faq" element={<FAQs />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
+import { GrCart, GrMenu, GrClose } from "react-icons/gr";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Navbar
+  return (
+    <div className="flex justify-between items-center px-4 py-2 shadow-md relative z-[999]">
+      <div className="">
+        <h1 className="text-primary-green font-semibold text-xl">Khana</h1>
+        <p className="text-primary-green  text-sm">Kidhr hai</p>
+      </div>
+      <div
+        className={`max-sm:absolute max-sm:flex max-sm:justify-center max-sm:shadow-lg max-sm:items-center max-sm:top-0 max-sm:h-screen  max-sm:w-[60%] max-sm:z-[-1] max-sm:left-0 max-sm:duration-300 bg-white ${
+          isOpen ? " max-sm:-translate-x-0 " : "max-sm:-translate-x-[100%]"
+        }`}
+      >
+        <ul className="flex items-center gap-4 max-sm:flex-col ">
+          <li className="cursor-pointer py-1 rounded-sm duration-300 hover:bg-primary-green hover:text-white ">
+            <Link to={"/"} className="px-3 py-2">
+              Home
+            </Link>
+          </li>
+          <li className="cursor-pointer  py-1 rounded-sm duration-300 hover:bg-primary-green hover:text-white ">
+            <Link to={"/about"} className="px-3 py-2">
+              About
+            </Link>
+          </li>
+          <li className="cursor-pointer py-1 rounded-sm duration-300 hover:bg-primary-green hover:text-white ">
+            <Link to={"/faq"} className="px-3 py-2">
+              FAQ
+            </Link>
+          </li>
+          <li className="cursor-pointer px-2 py-1  ">
+            <Link to={"/cart"}>
+              <GrCart size={"1.3em"} className="" />
+            </Link>
+          </li>
+          <li className="cursor-pointer px-2 py-1 rounded-sm duration-300 hover:bg-primary-green hover:text-white ">
+            <button>Login</button>
+          </li>
+        </ul>
+      </div>
+      <div onClick={() => setIsOpen(!isOpen)} className=" hidden max-sm:block">
+        {isOpen ? (
+          <GrClose size={"1.5em"} className="cursor-pointer" />
+        ) : (
+          <GrMenu size={"1.5em"} className="cursor-pointer" />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
